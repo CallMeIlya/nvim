0d1d651f983348991688b43444d574386c67e635
crystal <lee.kuznetsov@gmail.com>
2026-02-24 00:36:48 +0000

    my neovim configs

---
+++ init.lua
@@ -0,0 +1,18 @@
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("telescope").setup()
+require'nvim-treesitter'.setup {
+  -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+  install_dir = vim.fn.stdpath('data') .. '/site'
+}
+require'nvim-treesitter'.install { 'rust', 'javascript', 'zig', 'cpp', 'c' }
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'cpp', 'c', 'h' },
+  callback = function() vim.treesitter.start() end,
+})
+
+
+require("core.mapping")
+require("core.autocmd")
+print("core OK")
