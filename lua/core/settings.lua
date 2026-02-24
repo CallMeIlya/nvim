24a26eae5abcdfead6166018d085b2d2269d9aa2
crystal <lee.kuznetsov@gmail.com>
2026-02-24 00:44:53 +0000

    added settings

---
+++ lua/core/settings.lua
@@ -0,0 +1,7 @@
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath('config') .. '/undo'  -- Set undo directory
